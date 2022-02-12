c2cdb123b665dfd752d19d7e0f2595e3f27f806e
Cindy Confiant <cindy.confiant@gmail.com>
2022-02-12 11:38:12 +0100

    bgcolor work in progress

--- client/src/components/questions/Question.js
+++ client/src/components/questions/Question.js
@@ -10,6 +10,7 @@ class Question extends Component {
         response:null,
         solution:null,
         correct_answer: false,
+        isClicked: false,
     }
     
     componentDidMount(){
@@ -18,19 +19,25 @@ class Question extends Component {
 
     fetchAnswer= () => {
         solution(this.props.questions[this.state.currentQuestionIndex]._id)
-          .then(data => this.setState({solution: data.data.solution, correct_answer:data.data.correct_answer}))
+          .then(data => this.setState({solution: data.data.solution, correct_answer:data.data.correct_answer}, console.log('data', data)))
           .catch(err => this.setState({solution: null}))
        
     }
 
     handleClick= (index) => {
         this.setState(
-            {response: index}, () => {
-                if(this.state.response === this.state.solution) {
-                    this.setState({correct_answer:true})
-                }
+            {response: index, isClicked: true}, () => {
+               console.log('reponse', index) 
             }
         )   
+    }
+
+    handleCorrectAnswer = () => {
+        if(this.state.response === this.state.solution) {
+            this.setState({correct_answer:true}, () => {
+            console.log("correct_answer1",this.state.correct_answer)
+            })
+        }
     }
 
     handleNext= ()=>{ 
@@ -50,8 +57,8 @@ class Question extends Component {
 
     render(){
         const {questions, currentQuestionIndex} = this.state
-        console.log('this.state.questions', this.state.questions)
-
+        // console.log('this.state.questions', this.state.questions)
+        
         if(this.props.questions[currentQuestionIndex] === undefined) {
             return <Redirect to="/result" />
         }
@@ -73,14 +80,18 @@ class Question extends Component {
                 <h3>{title}</h3>
                 
                 {propositions.map((proposition, index) => {
+                    let btn = this.state.correct_answer ? "right" :"wrong"
+                    console.log('correct_answer2', this.state.correct_answer)
                     return (
                         <div key={index}>
                         
                          <button 
                           onClick={() => this.handleClick(index)}
                         //   style={{
-                        //      backgroundColor: index === solution ? "right":""
+                        //     // backgroundColor: this.state.correct_answer ? "red" : "blue"
+                            
                         //   }}
+                        className={btn}
                          >
                           {proposition} 
                          </button>
